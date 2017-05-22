6fe02bb3953df9e2c5b50d921a11cf35733a5fa9
Lucas Anselmo <lucasanselmo01@hotmail.com>
2017-05-22 10:14:02 -0300

    Foi criado um objeto da classe Produto e foi feito metodos para coleta de informações do formulario e passados para a classe Produto

--- PaginaCadastro.php
+++ PaginaCadastro.php
@@ -3,24 +3,40 @@
 <head><title>Pagina De Cadastro</title></head>
 <body>
 
+    <form method="post" name="CadastroProduto">
     <h1>Bem Vindo!!</h1>
     <h2>Cadastro de Produto</h2> <br/>
     <label>Marca: </label> <input type="text" name="marca"> <br/>
     <label>Descrição: </label> <input type="text" name="descricao"> <br/>
-    <label>Quantidade: </label> <input type="text" name="descricao"> <br/>
+    <label>Quantidade: </label> <input type="text" name="quantidade"> <br/>
     <label>Preço: </label> <input type="text" name="preco"> <br/>
     <input type="submit" name="Cadastrar">
+
+    </form>
 
     <?php
 
     require_once ("Produto.php");
+
+    $produto = new Produto();
+
+    $marca = $_POST['marca'];
+    $descricao = $_POST['descricao'];
+    $quantidade = $_POST['quantidade'];
+    $preco = $_POST['preco'];
+
+    $produto -> marca = $marca;
+    $produto -> descricao = $descricao;
+    $produto -> quantidade = $quantidade;
+    $produto -> preco = $preco;
+
 
 
 
 
     ?>
 
-</form>
+
 
 </body>
 </html>
